98e3861a25de47073087db235d56298998c3a3f0
shereensheer <Shereenahmed330@gmail.com>
2025-02-26 23:55:09 +0500

    python-assign

---
+++ app.py
@@ -0,0 +1,165 @@
+print("hello world")
+
+print ("shereen")
+
+teacher = "shahala"
+print (teacher)
+
+Father_Name: str = ("ahmed")
+print (Father_Name)
+
+Mothershag: bool = True
+print (Mothershag)
+
+marks = 90
+print (marks)   
+
+name : str ="sir Ameen"
+age:int =20
+is_active: bool = False
+
+print (name)
+print (age)
+print (is_active)
+
+#   ARTHMETIC
+# 7+5 = 12  Invalid syntax
+print(7+5)
+print (10-5)
+print (10 / 2)
+  
+#   5*2
+print (5*2)
+ 
+ #exponent 8^2 + 8x8
+
+print (8 ** 2)
+# floor division
+print (11 // 2)
+#  11 / 2 = 1 reminder / modulus
+print (11 % 2) 
+
+num : int =5
+num2 : int = 5
+
+# 5 + 5
+sum: int = num  + num2
+        #    5    -  5
+sum : int = num - num2   
+#           5    /  5
+div: int = num / num2
+#            5  x 5
+
+mul : int = num * num2
+print(mul)
+
+# Assigment operter
+
+
+x: int = 15
+# 15 + 5 = 15
+x = x + 5
+# x = x - 5
+# x = x * 2
+
+
+# x: int = 15
+# 15 * 2 = 30
+# reminer / modulus
+# x = x % 3
+print (x)
+
+   
+# 3. Comparison operators (True/False)
+#  ==   (equal to) 
+#  >    (greater than)
+#  <    (less than)
+#  >=   (greater or equal to)
+#  <=    (less than or equal to)
+#  !=    (not equal to)
+
+#  mera pehla operand/value  
+# dusre wale operand/value k baraber he
+
+num1: int = 10 
+num2: int = 11
+# print(10 == 10)
+print(num1 == num2)
+print(num2 > num1)
+print(num2 < num1)
+print(num1 >= num2 )
+print(num1 <= num2)
+print(num1 != num2)
+
+
+
+print("hello world!1")
+
+# def main():
+#     print("hello world from student")
+
+name : str = "Governor Intiative Artifical intelligence and computing"
+print( "GIAIC stand for : " ,name)
+
+# string
+# integer(number)
+# float(.decimal)
+# boolean
+# list(array)
+# tuple(imutible type similar list)
+# dictionary(object)
+# set 
+# non(none type)
+# frozen set 
+# byte array
+
+# String
+
+snake_case : str = "sir asharib ali"
+print(snake_case)
+
+id_card :str = """Name : Zari 
+Father_name : Ahmed"""
+print(id_card)
+
+
+# integar
+
+roll_num :int = 20
+print(type(roll_num))
+print(roll_num)
+
+# float
+
+float_num :float = 3.5
+print(float_num)
+
+
+# booleaan
+
+boolean :bool = False
+print(type(boolean))
+print(boolean)
+
+
+# list
+
+grocery: list[str] = ["apple", "cherry"]
+print(type(grocery))
+print(grocery[-1])
+
+
+# tuple
+
+tuple_data : tuple = ("mango", "stawberry")# immutabe jo change nhi hoti value
+print(tuple_data)
+
+# dictonary
+
+
+user_data : dict[str,str] = {
+    "name ": "hamza",
+    "fathername" : "syed"
+}
+print(user_data["fathername"])
+
